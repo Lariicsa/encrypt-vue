e46bf1d17f272a2f3d1f5eba0a1fb2f05956a38c
Larissa <dejamepienso@gmail.com>
2025-06-12 15:01:11 -0600

    Remove useless code

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex relative w-full justify-center items-center">
+    <header>
+      <div>
+
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <div class="flex relative w-full max-w-[980px]">
+      <RouterView />
+    </div>
+  </div>
+
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+	darkMode: 'class',
+	content: [
+		"./public/**/*.html",
+		"./src/**/*.{js,jsx,ts,tsx,vue}",
+	],
+
+	theme: {
+		extend: {
+			colors: {
+				"white-text": "#e0e0e0",
+				"white-opacity": "#807d7d",
+				magenta: "#8c2c7c",
+				"grey-1": "#282828",
+				"grey-2": "#363636",
+				"grey-3": "#1B202C",
+				"deg-peach": "#ffa984",
+				"deg-purple": "#c5a9f3",
+			},
+
+			fontfamily: {
+				sans: ["Kumbh Sans", ...defaultTheme.fontFamily.sans],
+			},
+		},
+		screens: {
+			xs: "475px",
+			// => @media (min-width: 475px) { ... }
+			sm: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
+
+			lg: "1025px",
+			// => @media (min-width: 1024px) { ... }
+
+			xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+
+			"2xl": "1536px",
+			// => @media (min-width: 1536px) { ... }
+		},
+		minHeight: {
+			"full-std": "calc(100vh - 116px)",
+		},
+	},
+};
